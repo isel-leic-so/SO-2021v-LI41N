a2cf7b0fc3c3ddc4cb0ada70f68839a68fd97e2d
Joao Trindade <jtrindade@cc.isel.ipl.pt>
2021-05-24 20:38:15 +0100

    16-pthreads

---
+++ 16-pthreads/pthreads03.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define NTHREADS 8
+#define NITERATIONS 100000
+
+pthread_mutex_t cnt_lock; // mutual exclusion
+int counter = 0;
+
+void * thread_func(void * arg) {
+	for (int i = 0; i < NITERATIONS; ++i) {
+		pthread_mutex_lock(&cnt_lock);
+		++counter;
+		pthread_mutex_unlock(&cnt_lock);
+	}
+	return NULL;
+}
+
+int main() {
+	pthread_mutex_init(&cnt_lock, NULL);
+	
+	pthread_t thread[NTHREADS];
+	
+	printf(":: START ::\n");
+	
+	for (int n = 0; n < NTHREADS; ++n) {
+		pthread_create(&thread[n], NULL, thread_func, NULL);
+	}
+	
+	printf(":: RUNNING ::\n");
+	
+	for (int n = 0; n < NTHREADS; ++n) {
+		pthread_join(thread[n], NULL);
+	}
+	
+	printf(":: DONE ::\n");
+
+	printf("Expected: %d\n", NTHREADS * NITERATIONS);
+	printf("Actual  : %d\n", counter);
+	
+	printf(":: END ::\n");
+	
+	pthread_mutex_destroy(&cnt_lock);
+	
+	return 0;
+}
